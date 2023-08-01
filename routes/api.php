4c4b94d55c34736c115b9728fdb047e9a00950b5
Peixotons <bielpcosta@hotmail.com>
2023-07-31 23:01:21 -0300

    chore: Update controllers, models and migrations

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\CompaniesController;
+use App\Http\Controllers\ContributorsController;
+use App\Http\Controllers\CustomersController;
+use App\Http\Controllers\FoodsController;
+use App\Http\Controllers\OrdersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +26,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResources([
+    'companies' => CompaniesController::class,
+    'admin' => AdminController::class,
+    'contributors' => ContributorsController::class,
+    'customers' => CustomersController::class,
+    'categories' => CategoriesController::class,
+    'orders' => OrdersController::class,
+    'food' => FoodsController::class,
+]);
